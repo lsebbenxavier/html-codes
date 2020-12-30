c5f38d759b98a64099c9d89c5daa943f8085528a
lsebbenxavier <lucas.s.xavier1@hotmail.com>
2020-12-30 16:09:26 -0300

    Update database

--- developmentproject/database_form.php
+++ developmentproject/database_form.php
@@ -2,12 +2,12 @@
    include_once ('connection.php');
 
    $nome = $_POST['nome'];
-   $replyto = $_POST['replyto']; // Email que será respondido
+   $replyto = $_POST['replyto']; 
    $mensagem_form = $_POST['mensagem'];
    $assunto = $_POST['serviço'];
 
    $resultdatabase_form "INSERT INTO database_form (nome, email, serviço , mensagem) VALUES (nome, email, serviço, mensagem)"
-   $mysqli_query ($conn, $resultdatabase_form) 
+   $resultadomysqli_query ($conn, $resultdatabase_form) 
 ?>
 
 
