734c3c052c84b24611facebe727e3db59fd14eb2
lsebbenxavier <lucas.s.xavier1@hotmail.com>
2021-01-02 07:43:53 -0300

    Update project

--- developmentproject/database_form.php
+++ developmentproject/database_form.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
    include_once ('connection.php');
    $nome = $_POST['nome'];
    $email = $_POST['email']; 
    $serviço = $_POST['serviço'];
    $mensagem = $_POST['mensage'];
-   
-   $result_database_form = "INSERT INTO database_form (nome, email, serviço, mensagem) VALUES ('$nome', '$email', '$serviço', '$mensagem')";
+
+   $result_database_form = "INSERT INTO database_form.php (nome, email, serviço, mensagem) VALUES ('$nome', '$email', '$serviço', '$mensagem')";
    $resultado_database_form= mysqli_query ($conn, $result_database_form); 
 ?>
 
