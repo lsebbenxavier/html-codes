bf41799333446e0155d751fbb2d1fd7f6eba5196
lsebbenxavier <lucas.s.xavier1@hotmail.com>
2021-01-06 20:30:49 -0300

    Update data base

--- developmentproject/connection.php
+++ developmentproject/connection.php
@@ -2,7 +2,7 @@
     $servidor = "localhost";
     $usuario = "root";
     $senha = ""; 
-    $dbname = "xdesignagency_qsl";
+    $dbname = "test";
     
     $conn = mysqli_connect ($servidor, $usuario, $senha, $dbname)
 ?>
