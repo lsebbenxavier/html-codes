e18295f97c2482b5b81738dd1a4cbf9eaa737c9d
lsebbenxavier <lucas.s.xavier1@hotmail.com>
2021-01-06 19:16:58 -0300

    Update php

--- developmentproject/connection.php
+++ developmentproject/connection.php
@@ -2,7 +2,7 @@
     $servidor = "localhost";
     $usuario = "root";
     $senha = ""; 
-    $dbname = "xdesignagency_sql";
+    $dbname = "xdesignagency_qsl";
     
     $conn = mysqli_connect ($servidor, $usuario, $senha, $dbname)
 ?>
--- developmentproject/database_form.php
+++ developmentproject/database_form.php
@@ -3,7 +3,7 @@
    $nome = $_POST['nome'];
    $email = $_POST['email']; 
    $serviço = $_POST['serviço'];
-   $mensagem = $_POST['mensage'];
+   $mensagem = $_POST['mensagem'];
 
    $result_database_form = "INSERT INTO database_form.php (nome, email, serviço, mensagem) VALUES ('$nome', '$email', '$serviço', '$mensagem')";
    $resultado_database_form= mysqli_query ($conn, $result_database_form); 
